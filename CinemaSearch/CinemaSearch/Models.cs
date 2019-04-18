9119cd68443b8bf035e7bd805c57f8dd89604fbd
jdeweese1 <jdeweese1@ksu.edu>
2019-04-18 13:35:21 -0500

    Add Models and Basic UI

---
+++ CinemaSearch/CinemaSearch/Models.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSearch
+{
+    class Movie
+    {
+        int MovieID;
+        string Title;
+        bool IsAdult;
+        int Runtime;
+        int ReleaseYear;
+    }
+    class MovieLanguage
+    {
+        int LanguageID;
+        int MovieID;
+    }
+    class Language
+    {
+        int LanguageID;
+        string Name;
+    }
+    class MovieRegion
+    {
+        int MovieID;
+        int RegionID;
+    }
+    class Region
+    {
+        int RegionID;
+        string Name;
+    }
+    class MovieGenre
+    {
+        int MovieID;
+        int GenreID;
+    }
+    class Genre
+    {
+        int GenreID;
+        string Name;
+    }
+    class Person
+    {
+        int PersonID;
+        string Name;
+        int BirthYear;
+    }
+    class Actor
+    {
+        int MovieID;
+        int PersonID;
+        string CharacterName;
+    }
+    class Director
+    {
+        int MovieID;
+        int PersonID;
+    }
+}
