b526f45b83ef5646c9363707d5fb7ada2492b893
jdeweese1 <jdeweese1@OBAMA2>
2019-04-22 20:00:06 -0500

    Add SQL executor class

--- CinemaSearch/CinemaSearch/MovieListView.Designer.cs
+++ CinemaSearch/CinemaSearch/MovieListView.Designer.cs
@@ -32,6 +32,7 @@
             this.uxMovieSearchbox = new System.Windows.Forms.TextBox();
             this.uxSearchTextLabel = new System.Windows.Forms.Label();
             this.uxSearchButtonGo = new System.Windows.Forms.Button();
+            this.uxMovieButtonMoreInfo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // uxMovieListBox
@@ -66,12 +67,24 @@
             this.uxSearchButtonGo.TabIndex = 3;
             this.uxSearchButtonGo.Text = "Go";
             this.uxSearchButtonGo.UseVisualStyleBackColor = true;
+            this.uxSearchButtonGo.Click += new System.EventHandler(this.uxSearchButtonGo_Click);
+            // 
+            // uxMovieButtonMoreInfo
+            // 
+            this.uxMovieButtonMoreInfo.Location = new System.Drawing.Point(142, 308);
+            this.uxMovieButtonMoreInfo.Name = "uxMovieButtonMoreInfo";
+            this.uxMovieButtonMoreInfo.Size = new System.Drawing.Size(75, 23);
+            this.uxMovieButtonMoreInfo.TabIndex = 4;
+            this.uxMovieButtonMoreInfo.Text = "More Info";
+            this.uxMovieButtonMoreInfo.UseVisualStyleBackColor = true;
+            this.uxMovieButtonMoreInfo.Click += new System.EventHandler(this.uxMovieButtonMoreInfo_Click);
             // 
             // MovieListView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uxMovieButtonMoreInfo);
             this.Controls.Add(this.uxSearchButtonGo);
             this.Controls.Add(this.uxSearchTextLabel);
             this.Controls.Add(this.uxMovieSearchbox);
@@ -89,5 +102,6 @@
         private System.Windows.Forms.TextBox uxMovieSearchbox;
         private System.Windows.Forms.Label uxSearchTextLabel;
         private System.Windows.Forms.Button uxSearchButtonGo;
+        private System.Windows.Forms.Button uxMovieButtonMoreInfo;
     }
 }
