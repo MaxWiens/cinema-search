3496ef0c28d6c523d984e24af386af9fd15f52c1
Max Wiens-Evangelista <rothmoss@gmail.com>
2019-04-25 12:35:50 -0500

    added importing from .mdf file

--- CinemaSearch/CinemaSearch/MovieListView.cs
+++ CinemaSearch/CinemaSearch/MovieListView.cs
@@ -12,9 +12,17 @@ namespace CinemaSearch
 {
     public partial class MovieListView : Form
     {
+
+        private SqlInterface _sqlinterface;
+
         public MovieListView()
         {
             InitializeComponent();
+
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True";
+
+            _sqlinterface = new SqlInterface(connectionString);
+            _sqlinterface.InitalizeDatabase();
         }
 
         private void uxSearchButtonGo_Click(object sender, EventArgs e)
@@ -23,12 +31,16 @@ namespace CinemaSearch
             if (b == null) throw new Exception("Object was not of type button");
             string userSearchText = b.Text;
             // run query looking for something with
-            var queryArgs = (title: userSearchText,);
+            Dictionary<string, string> queryDict = new Dictionary<string, string>();
+            queryDict.Add("title", userSearchText);
+
+            _sqlinterface.SearchByTitle(title:"bee movie");
+
         }
 
         private void uxMovieButtonMoreInfo_Click(object sender, EventArgs e)
         {
-
+            _sqlinterface.SearchByTitle("Bee");
         }
     }
 }
