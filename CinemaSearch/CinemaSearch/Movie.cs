9652aa2c3d064d054d1088f91a856e0b05cd3f58
Max Wiens-Evangelista <rothmoss@gmail.com>
2019-04-25 19:46:17 -0500

    created more UI elements

--- CinemaSearch/CinemaSearch/Movie.cs
+++ CinemaSearch/CinemaSearch/Movie.cs
@@ -8,23 +8,25 @@ namespace CinemaSearch
 {
     class Movie
     {
-        int MovieID;
-        string Title;
-        bool? IsAdult;
-        int? Runtime;
-        int? ReleaseYear;
-        Person Director;
+        public int MovieID { get; private set; }
+        public string Title { get; private set; }
+        public bool? IsAdult { get; private set; }
+        public int? Runtime { get; private set; }
+        public int? ReleaseYear { get; private set; }
+        public float? Rating { get; private set; }
+        public Person Director { get; private set; }
+        public List<AssociatedActor> Actors { get; private set; }
         //Genre Genre;
-        //Director Director;
 
-        public Movie(int movieID, string title, bool? isAdult, int? runTime, int? releaseYear)
+        public Movie(int movieID, string title, bool? isAdult, int? runTime, int? releaseYear, float? Rating)
         {
             MovieID = movieID;
             Title = title;
             IsAdult = isAdult;
             Runtime = runTime;
             ReleaseYear = releaseYear;
-            
         }
+
+        public override string ToString() => Title;
     }
 }
