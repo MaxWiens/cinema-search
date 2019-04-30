fc06ef4d1f7e41138c9186f8156e9006dc281889
Benjamin Cooper <benjaminc@ksu.edu>
2019-04-29 21:55:26 -0500

    Update Movie.AddMovie.sql

--- CinemaSearch/CinemaSearch/Sql/Procedures/Movie.AddMovie.sql
+++ CinemaSearch/CinemaSearch/Sql/Procedures/Movie.AddMovie.sql
@@ -1,10 +1,10 @@
 CREATE OR ALTER PROCEDURE Movie.AddMovie
 	@Title NVARCHAR(512),
-	@IsAsult BIT,
+	@IsAdult BIT,
 	@Runtime INT,
 	@ReleaseYear INT,
 	@StudioID INT,
 	@GenreID INT
 AS
 INSERT INTO Movie.Movies (MovieID, Title, IsAdult, Runtime, ReleaseYear, StudioID, GenreID)
-VALUES ((SELECT MAX(MovieID) FROM Movie.Movies)+1, @Title, @IsAsult, @Runtime, @ReleaseYear, @StudioID, @GenreID);
+VALUES ((SELECT MAX(MovieID) FROM Movie.Movies)+1, @Title, @IsAdult, @Runtime, @ReleaseYear, @StudioID, @GenreID);
